a92bf55528188c7a29f4c147da138764f61b2188
Chad <cahillone@gmail.com>
2014-04-06 15:57:27 -0700

    working on clean Bluetooth

--- src/ace/cab/ride/BacActivity.java
+++ src/ace/cab/ride/BacActivity.java
@@ -1,8 +1,50 @@
 package ace.cab.ride;
 
+import java.util.Set;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 
 public class BacActivity extends Activity {
+	private int REQUEST_ENABLE_BT = 1;
+	private BluetoothAdapter mBluetoothAdapter;
+	private ArrayAdapter<String> mArrayAdapter;
 	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bac);   
+        
+        setUpBluetooth(mBluetoothAdapter);
+        queryPairedDevices(mBluetoothAdapter);
+    }
+	
+	private void setUpBluetooth(BluetoothAdapter mBluetoothAdapter) {
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+		    // Device does not support Bluetooth
+		}
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+	}
+	
+	private void queryPairedDevices(BluetoothAdapter mBluetoothAdapter) {
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+		// If there are paired devices
+		if (pairedDevices.size() > 0) {
+		    // Loop through paired devices
+		    for (BluetoothDevice device : pairedDevices) {
+		        // Add the name and address to an array adapter to show in a ListView
+		        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+		    }
+		}
+	}
 }
